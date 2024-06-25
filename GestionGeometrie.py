b60602aadb67144359a3f59f70c12720fa76a428
amark <alexismarkiewicz020@gmail.com>
2024-06-25 10:43:47 -0400

    Correction du tracer géométrie, ne fonctionne pas encore bien, il faut corriger

--- GestionGeometrie.py
+++ GestionGeometrie.py
@@ -13,7 +13,7 @@ def calculer_coordonnées_coude(x_depart, y_depart, longueur, angle_deg, directi
     coordonnees_y = np.append(coordonnees_y, y_depart)
 
     angle_rad = np.deg2rad(angle_deg)
-    nbre_points_angle = longueur * 100
+    nbre_points_angle = int(longueur * 100)
 
     perimetre = longueur * 2 * np.pi / angle_rad
     rayon = perimetre / (2 * np.pi)
@@ -57,10 +57,11 @@ def calculer_coordonnees_guide(canalisation):
     liste_forme = np.array([])
     liste_angle = np.array([])
     for i in range(len(canalisation)):
-        liste_longueur = np.append(liste_longueur, int(canalisation[i][0]))
+        liste_longueur = np.append(liste_longueur, float(canalisation[i][0]))
         liste_forme = np.append(liste_forme, canalisation[i][4])
-        liste_angle = np.append(liste_angle, int(canalisation[i][5]))
+        liste_angle = np.append(liste_angle, float(canalisation[i][5]))
 
+    print(liste_longueur)
     longueur_canalisation = np.sum(liste_longueur)
 
     x_guide = np.array([])
@@ -73,31 +74,40 @@ def calculer_coordonnees_guide(canalisation):
         x_debut_troncon = x_guide[-1]
         y_debut_troncon = y_guide[-1]
 
+        longueur_troncon = liste_longueur[idx]
+
+        angle_troncon = liste_angle[idx]
+
         if i == 'droit':
             if idx > 0:
-                if liste_forme[i-1][-1] == 'D':
-                    for j in range(liste_longueur[idx]):
-                        x_guide = np.append(x_guide, x_debut_troncon + j)
+                if liste_forme[idx-1][-1] == 'D':
+                    for j in range(len(liste_longueur)):
+                        x_guide = np.append(x_guide, x_debut_troncon +  liste_longueur[j])
                         y_guide = np.append(y_guide, y_debut_troncon)
 
-                elif liste_forme[i-1][-1] == 'G':
-                    for j in range(liste_longueur[idx]):
-                        x_guide = np.append(x_guide, x_debut_troncon - j)
+                elif liste_forme[idx-1][-1] == 'G':
+                    for j in range(len(liste_longueur)):
+                        x_guide = np.append(x_guide, x_debut_troncon -  liste_longueur[j])
                         y_guide = np.append(y_guide, y_debut_troncon)
 
-                elif liste_forme[i-1][-1] == 'B':
-                    for j in range(liste_longueur[idx]):
+                elif liste_forme[idx-1][-1] == 'B':
+                    for j in range(len(liste_longueur)):
                         x_guide = np.append(x_guide, x_debut_troncon)
-                        y_guide = np.append(y_guide, y_debut_troncon - j)
+                        y_guide = np.append(y_guide, y_debut_troncon - liste_longueur[j])
 
-                elif liste_forme[i-1][-1] == 'H':
-                    for j in range(liste_longueur[idx]):
+                elif liste_forme[idx-1][-1] == 'H':
+                    for j in range(len(liste_longueur)):
                         x_guide = np.append(x_guide, x_debut_troncon)
-                        y_guide = np.append(y_guide, y_debut_troncon + j)
+                        y_guide = np.append(y_guide, y_debut_troncon +  liste_longueur[j])
+            else:
+                for j in range(len(liste_longueur)):
+                    x_guide = np.append(x_guide, x_debut_troncon)
+                    y_guide = np.append(y_guide, y_debut_troncon + liste_longueur[j])
 
         elif i == 'coude D' or i == 'coude G' or i == 'coude B' or i == 'coude H':
-            x, y = calculer_coordonnées_coude(x_debut_troncon, y_debut_troncon, liste_longueur[idx],
-                                              liste_angle[idx], i)
+            x, y = calculer_coordonnées_coude(x_debut_troncon, y_debut_troncon, float(liste_longueur[idx]),
+                                              float(liste_angle[idx]), i)
+            print(x,y)
             for j in range(len(x)):
                 x_guide = np.append(x_guide, x[j])
                 y_guide = np.append(y_guide, y[j])
@@ -105,7 +115,7 @@ def calculer_coordonnees_guide(canalisation):
         else:
             a = 1
 
-        return x_guide, y_guide
+    return x_guide, y_guide
 
 
 def tracer_canalisations(canalisation):
@@ -121,5 +131,4 @@ troncon3 = np.array([7, 'rond', 5, 2, 'droit', 90])
 canal = np.array([troncon1, troncon2, troncon3])
 
 tracer_canalisations(canal)
-
 
